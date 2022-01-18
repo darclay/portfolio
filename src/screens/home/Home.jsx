2fe740a91f8cb6623ea4d956146725ef01d20e58
David Gurule <davidgurulejr@gmail.com>
2022-01-18 11:43:23 -0700

    added projects instead of portfolio

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,47 +1,59 @@
-import './Home.css';
-import { Link } from 'react-router-dom'
-import windows from './photos/windows.jpg';
-import davidPic from './photos/david_pencil_sketch.png';
-import resumePic from './photos/resume.png';
-import portfolioPic from './photos/collision-course-black-white.png';
-import NavBar from '../../components/navigation/NavBar.jsx'
-import Footer from '../../components/footer/Footer.jsx'
+import "./Home.css";
+import { Link } from "react-router-dom";
+import windows from "./photos/windows.jpg";
+import davidPic from "./photos/david_pencil_sketch.png";
+import resumePic from "./photos/resume.png";
+import portfolioPic from "./photos/collision-course-black-white.png";
+import NavBar from "../../components/navigation/NavBar.jsx";
+import Footer from "../../components/footer/Footer.jsx";
 
 const Home = () => {
   return (
     <div className="homeDiv">
-        <NavBar />
+      <NavBar />
 
-      <Link to="meet-dave" style={{ textDecoration: 'none' }}>
+      <Link to="meet-dave" style={{ textDecoration: "none" }}>
         <div className="meetDavidDiv">
-        <img id="david" src={davidPic} alt="david" />
-        <h2 id="meetDave">Meet<br/>Dave</h2>
+          <img id="david" src={davidPic} alt="david" />
+          <h2 id="meetDave">
+            Meet
+            <br />
+            Dave
+          </h2>
         </div>
       </Link>
 
-        <hr className="separator"/>
+      <hr className="separator" />
 
-        <Link to="resume" style={{ textDecoration: 'none'}}>
+      <Link to="resume" style={{ textDecoration: "none" }}>
         <div className="davidResumeDiv">
-        <h2 id="davesResume">Dave's<br/>Resume</h2>
-        <img id="resume" src={resumePic} alt="resume" />
+          <h2 id="davesResume">
+            Dave's
+            <br />
+            Resume
+          </h2>
+          <img id="resume" src={resumePic} alt="resume" />
         </div>
-        </Link>
+      </Link>
 
-        <hr className="separator"/>
+      <hr className="separator" />
 
-        <Link to="projects" style={{ textDecoration: 'none'}}>
+      <Link to="projects" style={{ textDecoration: "none" }}>
         <div className="davidPortfolioDiv">
-        <img id="portfoliopic" src={portfolioPic} alt="portfolio" />
-        <h2 id="davesPortfolio">Dave's<br/>Portfolio</h2>
+          <img id="portfoliopic" src={portfolioPic} alt="portfolio" />
+          <h2 id="davesPortfolio">
+            Dave's
+            <br />
+            Projects
+          </h2>
         </div>
-        </Link>
+      </Link>
 
-        <hr className="separator"/>
+      <hr className="separator" />
 
-        <Footer /> 
+      <Footer />
     </div>
   );
-}
+};
 
-export default Home;             
+export default Home;
